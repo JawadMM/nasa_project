2f318e702db67fa44dc1fa097a5b5bf347ead27e
Jawad Almuttawa <almuttawa.jawad@gmail.com>
2025-04-06 13:21:25 +0300

    parsed planets data from the csv file to be displayed on the frontend.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,49 @@
-const planets = [];
+const fs = require("fs");
+const path = require("path");
+const { parse } = require("csv-parse");
 
-module.exports = planets;
+const HabitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          HabitablePlanets.push(data);
+        }
+      })
+      .on("error", (error) => {
+        console.log(error);
+      })
+      .on("end", () => {
+        // HabitablePlanets.map((planet) => {
+        //   const planetName = planet["kepler_name"];
+        //   console.log(planetName);
+        // });
+        console.log(HabitablePlanets.length);
+        resolve();
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: HabitablePlanets,
+};

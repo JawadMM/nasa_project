543edebe49982ce6d4efbad38abfa286e55fbbe7
JawadMM <almuttawa.j@hotmail.com>
2025-05-16 17:57:38 +0300

    Connected the current API calls with SPACE X API.

--- server/src/models/launches.mongo.js
+++ server/src/models/launches.mongo.js
@@ -5,11 +5,11 @@ const launchesSchema = new mongoose.Schema({
   mission: { type: String, required: true },
   rocket: { type: String, required: true },
   launchDate: { type: Date, required: true },
-  target: { type: String, required: true },
+  target: { type: String, required: false },
   customers: [String],
   upcoming: { type: Boolean, required: true },
   success: { type: Boolean, required: true, default: true },
 });
 
 // Connects LanchesSchema with the "launches" collection
-module.exports = mongoose.model("Launch", launchesSchema)
+module.exports = mongoose.model("Launch", launchesSchema);
